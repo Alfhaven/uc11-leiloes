618a89310c12adc3da8efd8d86d250570bb12918
Alvaro <freitas.alvaro0402@gmail.com>
2026-01-30 12:06:08 -0300

    Adicionado listagem dos produtos vendidos no vendasVIEW, e adicionado navegação interativa entre telas e botão de sair no cadastroVIEW

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -137,20 +137,23 @@ public class listagemVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
-        //*String id = id_produto_venda.getText();
+        String id = id_produto_venda.getText();
         
-        //ProdutosDAO produtosdao = new ProdutosDAO();
+        ProdutosDAO produtosdao = new ProdutosDAO();
         
-        //produtosdao.venderProduto(Integer.parseInt(id));
-        //listarProdutos();
+        produtosdao.venderProduto(Integer.parseInt(id));
+        listarProdutos();
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-        //vendasVIEW vendas = new vendasVIEW(); 
-        //vendas.setVisible(true);
+        vendasVIEW vendas = new vendasVIEW(); 
+        vendas.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
+        cadastroVIEW cadastro = new cadastroVIEW(); 
+        cadastro.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_btnVoltarActionPerformed
 
